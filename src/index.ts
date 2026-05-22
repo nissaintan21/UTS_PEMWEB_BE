d930427e45a85b016c76baea3e1f62317cc72307
Nissa Intan <nissaintan21@gmail.com>
2026-05-22 21:12:22 +0700

    update konfigurasi vercel

--- src/index.ts
+++ src/index.ts
@@ -5,57 +5,45 @@ import categoryRoute from "./routes/categoryRoute";
 import pembicaraRoute from "./routes/pembicaraRoute";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();  
+const prisma = new PrismaClient();  
 
-const app=express();
-const port=3000;
+const app = express();
+const port = process.env.PORT || 3000; // Menggunakan port dinamis dari hosting
 
 app.use(cors());
 app.use(express.json());
 
-
-app.get("/",(req,res)=>{
-    res.send("Hello,world!");
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
 });
 
-app.use("/events",eventRoute);
+app.use("/events", eventRoute);
 app.use("/categories", categoryRoute);
 app.use("/pembicara", pembicaraRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
 app.get("/dashboard", async (req, res) => {
+    try {
+        const totalEvent = await prisma.event.count();
+        const totalCategory = await prisma.category.count();
+        const totalPembicara = await prisma.pembicara.count();
 
-    try {
-
-        const totalEvent = await prisma.event.count();
-
-        const totalCategory = await prisma.category.count();
-
-        const totalPembicara = await prisma.pembicara.count();
-
-        res.json({
-            events: totalEvent,
-            categories: totalCategory,
-            pembicara: totalPembicara
-        });
-
-    } catch (error) {
-
-        console.log(error);
-
-        res.status(500).json({
-            message: "Terjadi Error"
-        });
-
-    }
-
+        res.json({
+            events: totalEvent,
+            categories: totalCategory,
+            pembicara: totalPembicara
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Terjadi Error"
+        });
+    }
 });
 
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
 
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+export default app;
